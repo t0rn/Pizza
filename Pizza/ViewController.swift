458834b47e522cd4295d6883156a4281bc1f3969
Alexey Ivanov <aleksey.ivanov1@gmail.com>
2023-03-04 11:45:42 +0100

    Initial Commit

---
+++ Pizza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pizza
+//
+//  Created by Alexey Ivanov on 4/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
